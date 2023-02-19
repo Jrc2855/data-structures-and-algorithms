f401053d53f9e9c6de83e9d2f44fd227d076058d
John <jrc2855@gmail.com>
2023-02-19 20:21:39 +0000

    Linked Lists

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,45 @@
 'use strict';
 
 class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
+  insert(value) {
+    let oldHead = this.head;
+    this.head = new Node(value);
+    this.head.next = oldHead;
+  }
+
+  toString() {
+    let current = this.head;
+    let text = '';
+    while (current) {
+      // do something
+      text += `{ ${current.value} } -> `;
+      current = current.next;
+    }
+    // return '{ banana } -> { apple } -> NULL'
+    return text + 'NULL';
+  }
+
+  includes(value) {
+    let current = this.head;
+    while(current) {
+      if(current.value === value) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+}
+
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 }
 
 module.exports = LinkedList;
